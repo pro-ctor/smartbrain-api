b994669d86df996fd8d1bba41e629004938d0f6a
pro-ctor <ethanproctor00@gmail.com>
2020-06-12 15:36:56 -0600

    added heroku database to knex connection

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'postgres',
-    database : 'smartbraindb'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
@@ -32,5 +32,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log(`App is running on port ${process.env.PORT}`);
+  console.log(`App has started successfully`);
 })
